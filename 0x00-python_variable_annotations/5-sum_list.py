8b5b62d18474df7bcf2ad95213869f34eb1547c9
IK Osula <you@example.com>
2024-04-18 14:41:00 +0000

    fix task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,7 +5,7 @@ Sums a list of floats
 from typing import List
 
 
-def sum_klist(input_list: List[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     '''
     Sums a list of floats
     '''
