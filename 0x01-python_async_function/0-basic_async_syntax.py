30011039fccf96f76565b3cc25a82bfcc0876432
IK Osula <you@example.com>
2024-04-23 15:55:44 +0000

    make fixes to modules

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -11,6 +11,6 @@ async def wait_random(max_delay: int = 10) -> float:
     Waits for a random number of seconds
     and returns the number of seconds
     '''
-    wait_time = random.random() * max_delay;
+    wait_time = random.random() * max_delay
     await asyncio.sleep(wait_time)
     return wait_time
